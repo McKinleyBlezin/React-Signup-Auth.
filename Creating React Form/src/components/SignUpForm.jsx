e8672e4ed38c8d7b1d88c7b00995ca35375b5134
McKinley Blezin <mblezin.usca@gmail.com>
2023-08-01 17:51:46 -0400

    first commit

---
+++ Creating React Form/src/components/SignUpForm.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+export default function SignUpForm({ token, setToken }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: username,
+            password: password,
+          }),
+        }
+      );
+      const result = await response.json();
+      setToken(result.token);
+      // console.log(result);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h2>Sign Up!</h2>
+      {error && <p>{error}</p>}
+      <form action="" onSubmit={handleSubmit}>
+        <label>
+          {" "}
+          Username
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          {" "}
+          Password
+          <input
+            type="text"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <button>
+          <input type="submit" />
+        </button>
+      </form>
+    </>
+  );
+}
